435684885da330b4157160e2a27c0e33e5da8a9c
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-01-06 11:16:20 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,18 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birth_date
-    
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" } do
-    validates :last_name
-    validates :first_name
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' } do
+      validates :last_name
+      validates :first_name
     end
 
-    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" } do
-    validates :last_name_kana
-    validates :first_name_kana
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+      validates :last_name_kana
+      validates :first_name_kana
     end
-   
   end
-  
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input both letters and numbers" }
+
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Input both letters and numbers' }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname { 'test' }
     email { 'test@examle' }
-    password { 'aaa111'}
-    password_confirmation {password}
+    password { 'aaa111' }
+    password_confirmation { password }
     last_name { 'てすと' }
     first_name { 'てすと' }
     last_name_kana { 'テスト' }
