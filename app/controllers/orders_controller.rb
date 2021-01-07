0192a398a2b9e5206b2b036c7c018867b301e79d
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-01-08 02:03:30 +0900

    フォームオブジェクトでのデータ保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,23 @@
 class OrdersController < ApplicationController
+  
   def index
     @user_order = UserOrder.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
